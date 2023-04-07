b1e732075d634c1e998b10f2661a4f827b1ab58d
sedhha <activity.schoolsh2@gmail.com>
2023-04-07 16:34:10 +0530

    Updated environment files

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,10 +1,17 @@
-import { cleanEnv, port, str } from 'envalid';
+import { z } from 'zod';
+import { logger } from '@utils/logger';
 
+const envSchema = z.object({
+  NODE_ENV: z.string(),
+  PORT: z.string(),
+  SUPERBASE_PROJECT_ID: z.string(),
+  SUPERBASE_PUBLIC_CLIENT_ID: z.string(),
+});
 export const ValidateEnv = () => {
-  cleanEnv(process.env, {
-    NODE_ENV: str(),
-    PORT: port(),
-    SUPERBASE_PROJECT_ID: str(),
-    SUPERBASE_PUBLIC_CLIENT_ID: str(),
-  });
+  const parsed = envSchema.safeParse(process.env);
+  const { success } = parsed;
+  if (!success) {
+    logger.error(JSON.stringify(parsed));
+    throw new Error('Unable to find environment variables');
+  } else logger.info('All Environment Variables Successfully Loaded');
 };
